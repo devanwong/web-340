d60a91e4e9c281c63552346f41c2d152a121edae
Devan Wong <devan.wong@yahoo.com>
2020-09-13 10:15:21 -0700

    submit ems

--- ems/app.js
+++ ems/app.js
@@ -8,23 +8,26 @@ console.log(header.display('Devan', 'Wong', 'EMS'));
 ; Description: week 5 & 6
 ;===========================================
 */
-//require express, ejs, routes and logger
+//require express, ejs, routes, mongo and logger
 var express = require("express");
 var http = require("http");
 var path = require("path");
 var logger = require("morgan");
 var app = express();
-
+var mongoose = require('mongoose');
 //ejs compiler
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 //morgan
 app.use(logger("short"));
+//including css
+app.use(express.static(__dirname + '/public'));
 //routes
 app.get("/", function(req, res) {
   res.render("index", {
-    title: "Home page",
-    message: "Devan's Landing page"
+    title: "Welcome to Fnames Employment Page",
+    message: "This is where you insert your employment and see everyone else's",
+    description: "This project is to showcase Node, Express and Mongo."
   });
 });
 //run server
