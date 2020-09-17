a611ab63e425b5a38ef7822dc2100dff5fa7b80b
Devan Wong <devan.wong@yahoo.com>
2020-09-17 15:17:54 -0700

    week 7 assignment 7.4

--- ems/app.js
+++ ems/app.js
@@ -4,8 +4,8 @@ console.log(header.display('Devan', 'Wong', 'EMS'));
 ============================================
 ; Title: EMS
 ; Author: Devan Wong
-; Date:   5 Sept 2020
-; Description: week 5 & 6
+; Date:   17 Sept 2020
+; Description: week 5, 6, 7
 ;===========================================
 */
 //require express, ejs, routes, mongo and logger
@@ -15,6 +15,24 @@ var path = require("path");
 var logger = require("morgan");
 var app = express();
 var mongoose = require('mongoose');
+var Employee = require("./models/employee.js");
+
+//mlab connection
+var mongoDB = "mongodb+srv://devthedev:november8@buwebdev-cluster-1.w20ui.mongodb.net/test";
+//creating the connections.
+mongoose.connect(mongoDB,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+  //useMongoClient: true deprecated.
+}).then(() => {
+  console.log("Connection to the database instance was successful");
+}).catch(err => {
+  console.log(`MongoDB error: ${err.message}`);
+});
+
+//adding promise
+mongoose.Promise = global.Promise;
+
 //ejs compiler
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
---
+++ ems/models/employee.js
@@ -0,0 +1,20 @@
+/*
+============================================
+; Title:  ems.js
+; Author: Devan Wong
+; Date:   17 September 2020
+; Description: File for the database model
+;===========================================
+*/
+//require statements
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//define employee schema
+let employeeSchema = new Schema({
+  firstName: String,
+  lastName: String
+});
+//expose employee to files.
+var Employee = mongoose.model('Employee', employeeSchema);
+module.exports = Employee;
